b04bf4b3808f339a07c3e6152c0f1d81520cfc13
sphiNx <12168474+sphinxr@user.noreply.gitee.com>
2024-08-26 13:09:12 +0500

    add tailwind and daisyui

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+// export default {
+//   content: ["./src/**/*.{html,ts,js}"],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [require('daisyui')],
+// }
+
+module.exports = {
+  content: ["./src/**/*.{html,ts,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [require('daisyui')],
+}
